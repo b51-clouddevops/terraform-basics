3a2821cbce02d2fd3c98fee570ebba644944b28e
mvpc-11232 <mvp2612@hotmail.com>
2022-12-05 08:34:19 +0530

    Auto Push Commit

--- example-infra/ec2.tf
+++ example-infra/ec2.tf
@@ -1,7 +1,8 @@
 # Creates EC2 Instance 
 resource "aws_instance" "app" {
-  ami           = "ami-0fa1ba08307b907ac"
-  instance_type = "t3.micro"
+  ami                        = "ami-0fa1ba08307b907ac"
+  instance_type              = "t3.micro"
+  vpc_security_group_ids     = [aws_security_group.allow_ssh]
 
   tags = {
     Name = "MyFirstTerraformInstance"
