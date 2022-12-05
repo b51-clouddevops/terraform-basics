87689132492d5d3b55dbf056f3385da5e8f5f01d
mvpc-11232 <mvp2612@hotmail.com>
2022-12-05 08:41:11 +0530

    asdf

--- example-infra/ec2.tf
+++ example-infra/ec2.tf
@@ -27,6 +27,14 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks      = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description      = "HTTP from Public"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
