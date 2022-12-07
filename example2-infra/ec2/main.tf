34718e9b4452b63d4fc244ba0a21a014bdb10f40
mvpc-11232 <mvp2612@hotmail.com>
2022-12-07 08:31:38 +0530

    Auto Push Commit

--- example2-infra/ec2/main.tf
+++ example2-infra/ec2/main.tf
@@ -3,6 +3,21 @@ resource "aws_instance" "app" {
   ami                        = data.aws_ami.myami.image_id
   instance_type              = "t3.micro"
   vpc_security_group_ids     = [var.sg]
+
+  connection {
+    type     = "ssh"
+    user     = "root"
+    password = var.root_password
+    host     = self.public_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "puppet apply",
+      "consul join ${aws_instance.web.private_ip}",
+    ]
+  }
+
 }
 
 
