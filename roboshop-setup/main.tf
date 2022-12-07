dc1852e873e1655ee1e5f67166e1282021720db4
mvpc-11232 <mvp2612@hotmail.com>
2022-12-07 08:48:11 +0530

    Auto Push Commit

--- roboshop-setup/main.tf
+++ roboshop-setup/main.tf
@@ -1,9 +1,10 @@
 module "frontend" {
     source      =   "./ec2"
     COMPONENT   =   "frontend"
+    APP_VERSION =   "0.0.2"
 }
 
-module "mongodb" {
-    source      =   "./ec2"
-    COMPONENT   =   "frontend"
-}
+# module "mongodb" {
+#     source      =   "./ec2"
+#     COMPONENT   =   "mongodb"
+# }
