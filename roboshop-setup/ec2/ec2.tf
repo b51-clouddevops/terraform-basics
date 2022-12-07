d11f1dc8bde48bd9821c84831b434c6815db0e75
mvpc-11232 <mvp2612@hotmail.com>
2022-12-07 08:47:07 +0530

    Auto Push Commit

--- roboshop-setup/ec2/ec2.tf
+++ roboshop-setup/ec2/ec2.tf
@@ -7,6 +7,20 @@ resource "aws_spot_instance_request" "cheap_worker" {
   tags = {
     Name = var.COMPONENT
   }
+
+  connection {
+    type     = "ssh"
+    user     = "centos"
+    password = "DevOps321"
+    host     = self.private_ip
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "ansible-pull -U https://github.com/b51-clouddevops/ansible.git -e ansible_user=centos -e ansible_password=DevOps321 -e COMPONENT=mongodb -e APP_VERSION=0.0.2 -e ENV=dev roboshop-pull.yml"
+    ]
+  }
+
 }
 
 
