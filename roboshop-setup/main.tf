f17183a6ca7cfaa1246df765c526bb6ba2e44830
mvpc-11232 <mvp2612@hotmail.com>
2022-12-07 08:51:28 +0530

    asdf

--- roboshop-setup/main.tf
+++ roboshop-setup/main.tf
@@ -1,10 +1,11 @@
-module "frontend" {
+# module "frontend" {
+#     source      =   "./ec2"
+#     COMPONENT   =   "frontend"
+#     APP_VERSION =   "0.0.2"
+# }
+
+module "mongodb" {
     source      =   "./ec2"
-    COMPONENT   =   "frontend"
+    COMPONENT   =   "mongodb"
     APP_VERSION =   "0.0.2"
 }
-
-# module "mongodb" {
-#     source      =   "./ec2"
-#     COMPONENT   =   "mongodb"
-# }
