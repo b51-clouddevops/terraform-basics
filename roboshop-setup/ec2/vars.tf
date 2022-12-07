26c4fb9ea8720d24650d78a68cee16ae37238015
mvpc-11232 <mvp2612@hotmail.com>
2022-12-07 09:08:29 +0530

    Auto Push Commit

--- roboshop-setup/ec2/vars.tf
+++ roboshop-setup/ec2/vars.tf
@@ -1,4 +1,10 @@
 variable "COMPONENT" {}
 variable "APP_VERSION" {}
 
-variable 
+variable "ALL_COMPONENTS" {
+    default = {
+        mongo   =  {
+            app_version  =  ""
+        }
+    }
+}
