c034d89bd4b834912bd70f0ac1cd4923e5c2ac10
mvpc-11232 <mvp2612@hotmail.com>
2022-12-07 08:55:22 +0530

    Auto Push Commit

--- roboshop-setup/ec2/route53.tf
+++ roboshop-setup/ec2/route53.tf
@@ -3,5 +3,5 @@ resource "aws_route53_record" "component" {
   name    = "${var.COMPONENT}-dev.robot.internal"
   type    = "A"
   ttl     = 10
-  records = [aws_spot_instance_request.lb.private_ip]
+  records = [aws_spot_instance_request.spot.private_ip]
 }
