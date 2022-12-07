49eed19a238a2ee9907e094aa2112c93568ee199
mvpc-11232 <mvp2612@hotmail.com>
2022-12-07 07:54:05 +0530

    Auto Push Commit

--- example2-infra/ec2/main.tf
+++ example2-infra/ec2/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "app" {
   ami                        = "ami-0fa1ba08307b907ac"
   instance_type              = "t3.micro"
-  vpc_security_group_ids     = [var.sg]
+#   vpc_security_group_ids     = [var.sg]
 
   tags = {
     Name = "MyFirstTerraformInstance"
