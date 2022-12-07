770c5fc460b3f1bedb7966b3fad62e8354c582cc
mvpc-11232 <mvp2612@hotmail.com>
2022-12-07 22:45:41 +0530

    asdf

--- roboshop-setup/ec2/ec2.tf
+++ roboshop-setup/ec2/ec2.tf
@@ -40,7 +40,7 @@ resource "aws_instance" "app" {
 
   provisioner "remote-exec" {
     inline = [
-      "ansible-pull -U https://github.com/b51-clouddevops/ansible.git -e ansible_user=centos -e ansible_password=DevOps321 -e COMPONENT=${var.COMPONENT} -e APP_VERSION=${var.APP_VERSION} -e ENV=dev roboshop-pull.yml"
+      "ansible-pull -U https://github.com/b51-clouddevops/ansible.git -e ansible_user=centos -e ansible_password=DevOps321 -e DB_PASSWORD=RoboShop@1 -e COMPONENT=${var.COMPONENT} -e APP_VERSION=${var.APP_VERSION} -e ENV=dev roboshop-pull.yml"
     ]
   }
 
