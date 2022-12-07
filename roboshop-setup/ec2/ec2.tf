2b5d3279965b478c4a6db1eaf258139fbada2cd9
mvpc-11232 <mvp2612@hotmail.com>
2022-12-07 08:54:53 +0530

    Auto Push Commit

--- roboshop-setup/ec2/ec2.tf
+++ roboshop-setup/ec2/ec2.tf
@@ -1,5 +1,5 @@
 # Request a spot instance at $0.03
-resource "aws_spot_instance_request" "cheap_worker" {
+resource "aws_spot_instance_request" "spot" {
   ami                          = "data.aws_ami.myami.image_id"
   instance_type                = "t3.micro"
   wait_for_fulfillment         = true
