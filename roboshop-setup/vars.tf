312f42d91a521d292c0ffc808f2ef9443528684d
mvpc-11232 <mvp2612@hotmail.com>
2022-12-07 09:21:03 +0530

    Auto Push Commit

--- roboshop-setup/vars.tf
+++ roboshop-setup/vars.tf
@@ -15,23 +15,18 @@ variable "ALL_COMPONENTS" {
         user   =  {
             app_version  =  "0.0.2"
         },
-
         mysql   =  {
             app_version  =  "null"
         },
-
         shipping   =  {
             app_version  =  "0.0.1"
         },
-
         rabbitmq   =  {
             app_version  =  "null"
         },
-
         payment   =  {
             app_version  =  "0.0.2"
         },
-
         frontend   =  {
             app_version  =  "0.0.2"
         },
