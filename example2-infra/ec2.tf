293c4aa669a0afedd013ae5edb28520f60650d05
mvpc-11232 <mvp2612@hotmail.com>
2022-12-07 07:41:38 +0530

    Auto Push Commit

--- example2-infra/ec2.tf
+++ example2-infra/ec2.tf
@@ -1,49 +1,49 @@
-# Creates EC2 Instance 
-resource "aws_instance" "app" {
-  ami                        = "ami-0fa1ba08307b907ac"
-  instance_type              = "t3.micro"
-  vpc_security_group_ids     = [aws_security_group.allow_ssh.id]
+# # Creates EC2 Instance 
+# resource "aws_instance" "app" {
+#   ami                        = "ami-0fa1ba08307b907ac"
+#   instance_type              = "t3.micro"
+#   vpc_security_group_ids     = [aws_security_group.allow_ssh.id]
 
-  tags = {
-    Name = "MyFirstTerraformInstance"
-  }
-}
+#   tags = {
+#     Name = "MyFirstTerraformInstance"
+#   }
+# }
 
-# Shows output on the screen
-output "private_dns" {
-    value   =   aws_instance.app.private_dns
-}
+# # Shows output on the screen
+# output "private_dns" {
+#     value   =   aws_instance.app.private_dns
+# }
 
-# Creates Security Group
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+# # Creates Security Group
+# resource "aws_security_group" "allow_ssh" {
+#   name        = "allow_ssh"
+#   description = "Allow SSH inbound traffic"
 
-  ingress {
-    description      = "SSH from VPC"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description      = "SSH from VPC"
+#     from_port        = 22
+#     to_port          = 22
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#   }
 
-  ingress {
-    description      = "HTTP from Public"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description      = "HTTP from Public"
+#     from_port        = 80
+#     to_port          = 80
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
+#   egress {
+#     from_port        = 0
+#     to_port          = 0
+#     protocol         = "-1"
+#     cidr_blocks      = ["0.0.0.0/0"]
+#     ipv6_cidr_blocks = ["::/0"]
+#   }
 
-  tags = {
-    Name = "allow_SSH"
-  }
-}
+#   tags = {
+#     Name = "allow_SSH"
+#   }
+# }
