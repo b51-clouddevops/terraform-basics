4f36aa6b9c37782e8b87c36caddce0b6684a5923
CodingManoj <mvp2612@hotmail.com>
2022-12-25 13:28:33 +0530

    wefdsx

--- 03-example1-infra/ec2.tf
+++ 03-example1-infra/ec2.tf
@@ -14,43 +14,35 @@ output "private_dns" {
     value   =   aws_instance.app.private_dns
 }
 
-locals {
-  ingress_inbound_rules = [
-    {
-      description      = "SSH from public"
-      from_port        = 22
-      to_port          = 22
-    },
-    {
-      description      = "HTTP from public"
-      from_port        = 80
-      to_port          = 80
-    },
-    {
-      description      = "HTTPS from public"
-      from_port        = 443
-      to_port          = 443
-    },
-  ]
-}
-
-
-
 # Creates Security Group
 resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
 
-  dynamic "ingress" {
-    for_each          = local.ingress_inbound_rules
-    content {
-      description      = ingress.value.description
-      from_port        = ingress.value.from_port
-      to_port          = ingress.value.to_port
+    ingress {
+      description      = Allow SSH From Public
+      from_port        = 22
+      to_port          = 22
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
     }
-  }
+  
+    ingress {
+      description      = Allow HTTP From Public
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+    ingress {
+      description      = Allow HTTPS From Public
+      from_port        = 443
+      to_port          = 443
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+
 
   egress {
     from_port        = 0
---
+++ 04-dynamic-blocks/ec2.tf
@@ -0,0 +1,60 @@
+# Creates EC2 Instance 
+resource "aws_instance" "app-server" {
+  ami                        = "ami-0fa1ba08307b907ac"
+  instance_type              = "t3.micro"
+  vpc_security_group_ids     = [aws_security_group.allow_ssh_http_https.id]
+
+  tags = {
+    Name = "MyFirstTerraformInstance"
+  }
+}
+
+locals {
+    ingress_inbound_rules = [
+        {
+            description      = "Allow SSH From Public"
+            from_port        = 22
+            to_port          = 22
+        },
+        {
+            description      = "Allow HTTP From Public"
+            from_port        = 80
+            to_port          = 80
+        },
+        {
+            description      = "Allow HTTPS From Public"
+            from_port        = 443
+            to_port          = 443
+        }
+    ]
+}
+
+
+# Creates Security Group
+resource "aws_security_group" "allow_ssh_http_https" {
+  name        = "allow_ssh_http_https"
+  description = "Allow SSH inbound traffic"
+
+  dynamic "ingress" {
+    for_each          = local.ingress_inbound_rules 
+    content   {
+      description      = ingress.description
+      from_port        = ingress.from_port
+      to_port          = ingress.to_port
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]      
+    }
+}
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh_http_https"
+  }
+}
