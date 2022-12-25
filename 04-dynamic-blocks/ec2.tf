7746acf1f2036f07498ce725d098543ab883fa2a
CodingManoj <mvp2612@hotmail.com>
2022-12-25 11:37:37 +0530

    Auto Push Commit

---
+++ 04-dynamic-blocks/ec2.tf
@@ -0,0 +1,53 @@
+# Creates EC2 Instance 
+resource "aws_instance" "app" {
+  ami                        = "ami-0fa1ba08307b907ac"
+  instance_type              = "t3.micro"
+  vpc_security_group_ids     = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "MyFirstTerraformInstance"
+  }
+}
+
+# Creates Security Group
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTP from Public"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "HTTPS from Public"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_SSH"
+  }
+}
+
