7fc533f0884ba3ede85f98e23096768939d779a6
CodingManoj <mvp2612@hotmail.com>
2022-12-25 13:29:17 +0530

    wefdsx

--- 04-dynamic-blocks/ec2.tf
+++ 04-dynamic-blocks/ec2.tf
@@ -38,9 +38,9 @@ resource "aws_security_group" "allow_ssh_http_https" {
   dynamic "ingress" {
     for_each          = local.ingress_inbound_rules 
     content   {
-      description      = ingress.description
-      from_port        = ingress.from_port
-      to_port          = ingress.to_port
+      description      = ingress.value.description
+      from_port        = ingress.value.from_port
+      to_port          = ingress.value.to_port
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]      
     }
